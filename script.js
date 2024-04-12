ddba4b9080afa1f4e01e5aee23bd783cea4ec115
Jyakaru <143911905+Jyakaru@users.noreply.github.com>
2024-04-12 18:59:35 -0500

    Update script.js
    doc.write

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // This is the JS file named, script.js for the project
-function hello(name){
-  console.log("Hello " + name + "! From the js file!");
-}
-hello("world");
+
+console.log("Hello " + name + "! From the js file!");
+
+document.write("Hello from the js file");
