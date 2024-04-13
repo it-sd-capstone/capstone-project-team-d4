2a45795c6eb11219514fde60c331bd4d066c41f2
zygaant <157663027+zygaant@users.noreply.github.com>
2024-04-13 00:07:19 +0000

    adds canvas object via javascript

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 console.log("Hello " + name + "! From the js file!");
 
 document.write("Hello from the js file");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "blue";
+ctx.fillRect(10, 10, 150, 100);
