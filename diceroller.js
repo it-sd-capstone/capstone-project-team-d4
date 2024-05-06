f695bf5b7fb365f17cf46b55ccc870cb273dff4d
Jacky Yang <jyang25@cvtc.student.cvtc.edu>
2024-05-06 16:44:23 -0500

    added a check for only numbers in the boxes

--- diceroller.js
+++ diceroller.js
@@ -3,10 +3,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(10, 10, 150, 100);
 
 // create a custom dice roller for two different dice faces with x number of dice roll for each dice face variation in one roll.
+
+
+
+
+
+function testingRollDice(){
+  let digit = document.getElementById("")
+  let numberOfDice = document.getElementById("numOfDice" + digit)
+  for(let i = 0; i < numOfDice; i++){
+    console.log("hi")
+  }
+}
+
+
+
 
 function rollDice4(){  
     let numOfDice = document.getElementById("numOfDice4").value;
@@ -18,14 +31,14 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult4.textContent = "please input a number to continue"; 
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult4.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
       diceResult4.textContent = "Die: " + values + " Total: " + total;
     } else {
       diceResult4.textContent = "Dice: " + values + " Total: " + total;
     }
+    
   }
 
   function rollDice6(){   
@@ -38,9 +51,8 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult6.textContent = "please input a number to continue"; 
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult6.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
         diceResult6.textContent = "Die: " + values + " Total: " + total;
     } else {
@@ -58,9 +70,8 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult8.textContent = "please input a number to continue";
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult8.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
         diceResult8.textContent = "Die: " + values + " Total: " + total;
     } else {
@@ -78,9 +89,8 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult10.textContent = "please input a number to continue";
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult10.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
         diceResult10.textContent = "Die: " + values + " Total: " + total;
     } else {
@@ -98,9 +108,8 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult12.textContent = "please input a number to continue";
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult12.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
       diceResult12.textContent = "Die: " + values + " Total: " + total;
     } else {
@@ -118,9 +127,8 @@ function rollDice4(){
       values.push(value);
       total += value;
     }
-    if(numOfDice == String){ // doesn't work yet
-      document.write("asd")
-      diceResult20.textContent = "please input a number to continue";
+    if(numOfDice == isNaN || numOfDice <= 0){ // doesn't work yet
+      diceResult20.textContent = "please input a valid number greater than 0"; 
     } else if (numOfDice == 1){
         diceResult20.textContent = "Die: " + values + " Total: " + total;
     } else {
@@ -130,9 +138,20 @@ function rollDice4(){
 
   // not prio rn
 function customRoll(numOfFirstDie, numOfFacesFirstDie, numOfSecDie, numOfFacesSecDie){
+// rest utton 
+// total
+// one roll button
 
+// ds array of dice, stores which dice I want to roll
 
+//let somearray = [4,4,6]
+// new function called rolldice?
+// creates a new 
 }
+
+
+
+
 // function test(numberOfDice, numFaces){
     
 //     //let numberOfDice = document.getElementById("numOfDice").value;
